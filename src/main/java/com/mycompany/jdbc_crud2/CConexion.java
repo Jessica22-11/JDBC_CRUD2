22b093f5b7e08386cce2446817eb81efbb6cb6e3
Jessica Alfonso <taticalinda221@gmail.com>
2025-04-03 10:20:51 -0500

    Se agrego el objeto para mostrar formnulario

--- src/main/java/com/mycompany/jdbc_crud2/CConexion.java
+++ src/main/java/com/mycompany/jdbc_crud2/CConexion.java
@@ -4,10 +4,39 @@
  */
 package com.mycompany.jdbc_crud2;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author tatic
  */
 public class CConexion {
+    
+    Connection conectar = null;
+    
+    String usuario ="root";
+    String Contrasenia = "1234Sena";
+    String bd = "dbescuelas";
+    String ip ="localhost";
+    String puerto = "3306";
+    
+    String cadena = "jdbc:mysql://"+ip+":"+puerto+"/"+bd;
+    
+    public Connection establecerConexion() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            conectar = DriverManager.getConnection(cadena,usuario,Contrasenia);
+            
+            JOptionPane.showMessageDialog(null, "La conexion se relaizo con exito");
+            
+        } catch (Exception e) {
+            
+            JOptionPane.showMessageDialog(null, "Error al conectrase con la base de datos" + e.toString());
+        }
+    return conectar;
+    }
     
 }
